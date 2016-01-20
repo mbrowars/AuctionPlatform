f5de5bb6ca38b6b927ab2fe03fdb3be69bc67e7c
Göppi <max.goeppert@yahoo.de>
2016-01-20 19:28:05 +0100

    Beans und Hibernate-Mapping-Files für Tabellen Auction und User hinzugefügt, Hibernate Config hinzugefügt

--- AuctionPlatform/src/de/ba/AuctionPlatform/dao/AuctionDao.java
+++ AuctionPlatform/src/de/ba/AuctionPlatform/dao/AuctionDao.java
@@ -6,6 +6,61 @@
  */
 package de.ba.AuctionPlatform.dao;
 
-public class AuctionDao {
+import java.sql.Date;
 
+public class AuctionDao {
+	Long id;
+	String titel;
+	Double gebot;
+	Date enddatum;
+	String beschreibung;
+	Long hoechstbietenderid;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitel() {
+		return titel;
+	}
+
+	public void setTitel(String titel) {
+		this.titel = titel;
+	}
+
+	public Double getGebot() {
+		return gebot;
+	}
+
+	public void setGebot(Double gebot) {
+		this.gebot = gebot;
+	}
+
+	public Date getEnddatum() {
+		return enddatum;
+	}
+
+	public void setEnddatum(Date enddatum) {
+		this.enddatum = enddatum;
+	}
+
+	public String getBeschreibung() {
+		return beschreibung;
+	}
+
+	public void setBeschreibung(String beschreibung) {
+		this.beschreibung = beschreibung;
+	}
+
+	public Long getHoechstbietenderid() {
+		return hoechstbietenderid;
+	}
+
+	public void setHoechstbietenderid(Long hoechstbietenderid) {
+		this.hoechstbietenderid = hoechstbietenderid;
+	}
 }
--- AuctionPlatform/src/de/ba/AuctionPlatform/dao/UserDao.java
+++ AuctionPlatform/src/de/ba/AuctionPlatform/dao/UserDao.java
@@ -7,5 +7,40 @@
 package de.ba.AuctionPlatform.dao;
 
 public class UserDao {
+	Long id;
+	String email;
+	String code;
+	String ip;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
 }
