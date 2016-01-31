a531b0c51416d4caf35b6dcf604bd95100d0c0de
Lenco <markus_froehlich96@yahoo.de>
2016-01-31 21:57:26 +0100

    Ajax Update

--- AuctionPlatform/WebContent/js/auction.js
+++ AuctionPlatform/WebContent/js/auction.js
@@ -5,15 +5,19 @@ $(function() {
 			  type: "POST",
 			  url: "/AuctionPlatform/auction",
 			  data: { 'mail': $("#mail").val(),
-				  	  'bid' : $("#bid").val()}
-			})
-			  .success(function() {
-				$(".modal-body span").html("<span style='color: green;'> E-Mail wurde erfolgreich versendet!</span>")
-				$(".modal-body p").html('<label>Code</label><input placeholder="Bestätigungscode" id="code"><button id="testCode" class="ourButton">Senden</button>')
-			})
-			  .error(function(a,b,c) {
-				$(".modal-body span").html("<span style='color: red;'> E-Mail konnte nicht versendet werden!" + a + b + c +"</span>")
-			});
+				  	  'bid' : $("#bid").val()},
+
+			  beforeSend: function(){
+				$(".modal-body span").html('<img src="img/spinner.gif">');  
+			  },
+			  success: function() {
+				$(".modal-body span").html("<span style='color: green;'> E-Mail wurde erfolgreich versendet!</span>");
+				$(".modal-body p").html('<label>Code</label><input placeholder="Bestätigungscode" id="code"><button id="testCode" class="ourButton">Senden</button>');
+			},
+			  error: function() {
+				$(".modal-body span").html("<span style='color: red;'> E-Mail konnte nicht versendet werden!</span>");
+			}
+		});
 		
 		
 		$("#bid").val("");
@@ -28,14 +32,19 @@ $(function() {
 $('body').on('click','#testCode',function(){
 	$.ajax({
 		  method: "POST",
-		  url: "auction",
-		  data: { code: $("#code").val() }
-		})
-		  /*.success(function() {
-		    $(".modal-body p").html("<span style='color: green;' E-Mail wurde erfolgreich versendet!</span>")
-		  });*/
-	
-	$(".modal-body p").html('<span style="color: green;"> Gebot wurde erfolgreich bestätigt!</span>');
+		  url: "/AuctionPlatform/auction",
+		  data: { code: $("#code").val() },
+		
+		  beforeSend: function(){
+			$(".modal-body p").html('<img src="img/spinner.gif">');  
+		  },
+		  success: function() {
+		    $(".modal-body p").html("<span style='color: green;'>Gebot wurde erfolgreich bestätigt!</span>");
+		  },
+		  error:  function() {
+		    $(".modal-body p").html("<span style='color: red;'>Fehler: Gebot wurde nicht bestätigt.</span>");
+		  }
+	});
 	
 	$("#bid").val("");
 });
