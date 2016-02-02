5c5b184738519b26089cbe2705e3260e8af9979c
mutzle420 <matthibrow@googlemail.com>
2016-02-02 10:59:23 +0100

    änderungen Auctionmehtods & Usermethods

--- AuctionPlatform/src/de/ba/AuctionPlatform/dao/AuctionMethods.java
+++ AuctionPlatform/src/de/ba/AuctionPlatform/dao/AuctionMethods.java
@@ -4,6 +4,9 @@ import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -13,77 +16,107 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.junit.Test;
 
+import de.ba.AuctionPlatform.controller.CreateAuctionServlet;
 import de.ba.AuctionPlatform.dao.Auction;
 
 public class AuctionMethods {
 	private static SessionFactory factory;
-	private static ServiceRegistry serviceRegistry;
-	
-	/*Auktion anlegen*/
-	public Long addAuction(Long auctionid, String titel, Double gebot, String enddatum, String beschreibung, Long hoechstbietenderid, Blob picture) {
+	private static final Logger logger = Logger.getLogger(CreateAuctionServlet.class);
+
+	/* Auktion anlegen */
+
+	/**
+	 * @param auctionid
+	 * @param titel
+	 * @param gebot
+	 * @param enddatum
+	 * @param beschreibung
+	 * @param hoechstbietenderid
+	 * @param picture
+	 * @return
+	 */
+	public Long addAuction(Long auctionid, String titel, Double gebot, String enddatum, String beschreibung,
+			Long hoechstbietenderid, Blob picture) {
 		Session session = factory.openSession();
 		Transaction tx = null;
-		Long auctionidSaved = null;
+
 		try {
 			tx = session.beginTransaction();
 			Auction auction = new Auction(auctionid, titel, gebot, enddatum, beschreibung, hoechstbietenderid, picture);
-			auctionidSaved = (Long) session.save(user);
+			session.save(auction);
 			tx.commit();
+			logger.log(Level.INFO, "Auktion: " + auctionid + "," + titel + " wurde angelegt.");
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
+			logger.log(Level.ERROR, "Auktion: " + auctionid + "," + titel + " konnte nicht angelegt werden " + e);
 		} finally {
 			session.close();
+
 		}
-		return useridSaved;
+		return auctionid;
 	}
-	
+
 	/* Auktion löschen */
+	/**
+	 * @param auction
+	 */
 	public void removeAuction(Auction auction) {
 		Session session = factory.openSession();
 		Transaction tx = null;
-		
+
 		try {
 			tx = session.beginTransaction();
 			session.delete(auction);
 			tx.commit();
-												
+			logger.log(Level.INFO, "Auktion: " + auction + " wurde angelöscht.");
+
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
+			logger.log(Level.ERROR, "Auktion: " + auction + " konnte nicht gelöscht werden." + e);
 		} finally {
 			session.close();
 		}
 	}
-	
-	/*Auktion ändern*/
+
+	/* Auktion ändern */
+	/**
+	 * @param auction
+	 */
 	public void updateAuction(Auction auction) {
 		Session session = factory.openSession();
 		Transaction tx = null;
-		
+
 		try {
 			tx = session.beginTransaction();
 			session.update(auction);
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
+			logger.log(Level.ERROR, "Auktion: " + auction + " konnte nicht aktualisiert werden." + e);
 		} finally {
 			session.close();
 		}
 	}
-	
+
+	/**
+	 * @return
+	 */
 	public List getAllAuctions() {
 		Session session = factory.openSession();
 		Transaction tx = null;
 		List auctions = new ArrayList();
 		try {
 			tx = session.beginTransaction();
-			auctions = (List)session.createQuery("FROM Auction").list();
+			auctions = (List) session.createQuery("FROM Auction").list();
 			tx.commit();
+			logger.log(Level.INFO, "Index.jsp wurde aktualisiert.");
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
 			e.printStackTrace();
+			logger.log(Level.ERROR, "Es konnten keine Auktionen geladen werden." + e);
 		} finally {
 			session.close();
 		}
--- AuctionPlatform/src/de/ba/AuctionPlatform/dao/UserMethods.java
+++ AuctionPlatform/src/de/ba/AuctionPlatform/dao/UserMethods.java
@@ -3,6 +3,9 @@ package de.ba.AuctionPlatform.dao;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -12,80 +15,108 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.junit.Test;
 
+import de.ba.AuctionPlatform.controller.CreateAuctionServlet;
 import de.ba.AuctionPlatform.dao.User;
 
+/**
+ * @author
+ *
+ */
 public class UserMethods {
 
 	private static SessionFactory factory;
-	private static ServiceRegistry serviceRegistry;
-	
-	/*User anlegen*/
+	private static final Logger logger = Logger.getLogger(CreateAuctionServlet.class);
+
+	/**
+	 * @param userid
+	 * @param email
+	 * @param code
+	 * @param ip
+	 * @return
+	 */
+	/* User anlegen */
 	public Long addUser(Long userid, String email, String code, String ip) {
 		Session session = factory.openSession();
 		Transaction tx = null;
-		Long useridSaved = null;
+
 		try {
 			tx = session.beginTransaction();
 			User user = new User(userid, email, code, ip);
-			useridSaved = (Long) session.save(user);
+			session.save(user);
 			tx.commit();
+			logger.log(Level.INFO, "User: " + userid + "," + email + " wurde angelegt.");
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
+			logger.log(Level.ERROR, "User: " + userid + "," + email + " konnte nicht angelegt werden." + e);
 		} finally {
 			session.close();
 		}
-		return useridSaved;
+		return userid;
 	}
-	
+
 	/* User löschen */
+	/**
+	 * @param user
+	 */
 	public void removeUser(User user) {
 		Session session = factory.openSession();
 		Transaction tx = null;
-		
+
 		try {
 			tx = session.beginTransaction();
 			session.delete(user);
 			tx.commit();
-												
+			logger.log(Level.INFO, "User: " + user + " wurde gelöscht.");
+
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
+			logger.log(Level.ERROR, "User: " + user + " konnte nicht gelöscht werden." + e);
 		} finally {
 			session.close();
 		}
 	}
-	
-	/*User ändern*/
+
+	/* User ändern */
+	/**
+	 * @param user
+	 */
 	public void updateUser(User user) {
 		Session session = factory.openSession();
 		Transaction tx = null;
-		
+
 		try {
 			tx = session.beginTransaction();
 			session.update(user);
+			logger.log(Level.INFO, "User: " + user + " wurde aktualisiert.");
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
+			logger.log(Level.ERROR, "User: " + user + " konnte nicht aktualisiert werden." + e);
 		} finally {
 			session.close();
 		}
 	}
-	
-	/*alle User aufzählen*/
+
+	/* alle User aufzählen */ // @Göppi brauchen wir nicht wirklich :D
+
+	/**
+	 * @return
+	 */
 	public List getAllUsers() {
 		Session session = factory.openSession();
 		Transaction tx = null;
 		List users = new ArrayList();
 		try {
 			tx = session.beginTransaction();
-			users = (List)session.createQuery("FROM User").list();
+			users = (List) session.createQuery("FROM User").list();
 			tx.commit();
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
 			e.printStackTrace();
-		} finally {
+			logger.log(Level.ERROR, "User konnten nicht ausgelesen werden." + e);
 			session.close();
 		}
 		return users;
