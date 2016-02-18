69cb922add89baa3fa5845939352e8ebd3d4abc8
mutzle420 <matthibrow@googlemail.com>
2016-02-18 09:10:11 +0100

    Loop für Auktionen in Testklasse @Markus kannst dir ja damit was zusammen bauen

--- AuctionPlatform/src/de/ba/AuctionPlatform/controller/AuctionPlatformServlet.java
+++ AuctionPlatform/src/de/ba/AuctionPlatform/controller/AuctionPlatformServlet.java
@@ -30,25 +30,28 @@ public class AuctionPlatformServlet extends HttpServlet {
 	static final long serialVersionUID = 1L;
 	private static final Logger logger = Logger.getLogger(AuctionPlatformServlet.class);
 
-	public void init() {
-		try {
-
-			HibernateUtil hibernate = HibernateUtil.getInstance();
-			logger.log(Level.INFO, "Initializing Hibernate");
-		} catch (HibernateException ex) {
-			logger.log(Level.INFO, ex);
-			System.exit(5);
-		}
-	}
+	// public void init() {
+	// try {
+	//
+	// HibernateUtil hibernate = HibernateUtil.getInstance();
+	// logger.log(Level.INFO, "Initializing Hibernate");
+	// } catch (HibernateException ex) {
+	// logger.log(Level.INFO, ex);
+	// System.exit(5);
+	// }
+	// }
 
 	@Override
 	public void doGet(HttpServletRequest requ, HttpServletResponse resp) throws ServletException, IOException {
-
+		HttpSession session = requ.getSession(true);
 		try {
 
 			HibernateUtil hibernate = HibernateUtil.getInstance();
 			logger.log(Level.INFO, "Initializing Hibernate");
 			List auction = AuctionDAO.getAllAuctions();
+			session.setAttribute("auctions", auction);
+			AuctionPlatformServletTest t = new AuctionPlatformServletTest();
+			t.listTest(auction);
 
 		} catch (HibernateException ex) {
 			logger.log(Level.INFO, ex);
--- AuctionPlatform/test/de/ba/AuctionPlatform/controller/AuctionPlatformServletTest.java
+++ AuctionPlatform/test/de/ba/AuctionPlatform/controller/AuctionPlatformServletTest.java
@@ -7,21 +7,32 @@
 package de.ba.AuctionPlatform.controller;
 
 import java.io.*;
+import java.util.List;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.websocket.Session;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import de.ba.AuctionPlatform.dao.Auction;
 
 /**
  * 
  * @author Matthias Browarski
  *
  */
+
 public class AuctionPlatformServletTest extends HttpServlet {
+
 	static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger.getLogger(AuctionPlatformServletTest.class);
 
-	@Override
-	public void doGet(HttpServletRequest requ, HttpServletResponse resp) throws ServletException, IOException {
+	public void listTest(List<Auction> auction) {
 
-		//TODO Testfall erstellen
-
+		for (Auction auc : auction) {
+			System.out.println(auc.getId() + " , " + auc.getTitel() + "," + auc.getGebot());
+		}
 	}
 }
