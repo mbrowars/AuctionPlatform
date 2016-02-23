a972dfeff2f845c1f94caf1377d5acfe7ecc0b12
Matthias <Matthias@Matthias-PC.home>
2016-02-23 22:17:18 +0100

    göppi und seine fehler <3

--- AuctionPlatform/test/de/ba/AuctionPlatform/dao/AuctionDaoTest.java
+++ AuctionPlatform/test/de/ba/AuctionPlatform/dao/AuctionDaoTest.java
@@ -11,31 +11,28 @@ import java.sql.*;
 import de.ba.AuctionPlatform.dao.Auction;
 
 public class AuctionDaoTest {
-	//TODO Testfall erstellen
+	// TODO Testfall erstellen
 	Auction auction1 = new Auction("titel", 24.50, (long) 1234235, "beschreibung", 12, null);
 	Auction auction2 = new Auction("SNES", 123.45, (long) 32546, "Old Stuff with style (new Update)", 4, null);
-	auction2.setAuctionid(2);
-	
-	
-	
+
 	public void addAuction() {
 		AuctionDAO.addAuction(auction1);
+		auction2.setAuctionid(2);
 		AuctionDAO.addAuction(auction2);
 	}
-	
-	
+
 	public void getAuction() {
 		AuctionDAO.getAuction(3);
 	}
-	
-	
+
 	public void removeAuction() {
 		AuctionDAO.removeAuction(16);
 		AuctionDAO.removeAuction(17);
 	}
-	
+
 	@Test
 	public void updateAuction() {
+		auction2.setAuctionid(2);
 		AuctionDAO.updateAuction(auction2);
 	}
 }
